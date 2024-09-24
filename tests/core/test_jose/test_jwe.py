64c2f780610af140a65830deb2fb2391fdbb85a6
Alon Ohana <alon.o@sealsecurity.io>
2024-09-24 14:05:23 +0300

    begste

--- tests/core/test_jose/test_jwe.py
+++ tests/core/test_jose/test_jwe.py
@@ -249,8 +249,7 @@ class JWETest(unittest.TestCase):
     def test_import_unsafe_key():
         self.assertRaises(
             ValueError,
-            OctKey.import_key,
-            "ssh-rsa UNSAFE"
+            OctKey.import_key("ssh-rsa UNSAFE")
         )
 
     def test_dir_alg(self):
